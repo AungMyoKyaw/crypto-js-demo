d7b1e3ebabb04129f5e6784586fc94ee80f8b8e1
Aung Myo Kyaw <git@aungmyokyaw.anonaddy.me>
2022-11-06 01:35:41 +0630

    feat: footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
   import NavBar from './lib/NavBar.svelte';
   import CryptoJS from './lib/CryptoJS.svelte';
+  import Footer from './lib/Footer.svelte';
 </script>
 
 <main>
   <div>
     <NavBar />
     <CryptoJS />
+    <Footer />
   </div>
 </main>
 
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<div class="text-center" style="font-size:.8em">
+  <p class="text-secondary text-uppercase fw-normal mb-1">
+    Demo for
+    <a
+      href="https://github.com/brix/crypto-js"
+      target="_blank"
+      class="text-decoration-none text-reset">brix/crypto-js</a
+    >
+  </p>
+  <p class="text-secondary text-uppercase fw-normal mb-4">
+    Made with 🌸 in Burma
+  </p>
+</div>
